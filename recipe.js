c1f3aaaf2c5172039a8f91f21f28670ac78e0597
uinjei <javier.edwinjay@gmail.com>
2018-08-28 19:06:35 +0800

    Create recipe.js

---
+++ recipe.js
@@ -0,0 +1,36 @@
+const addMilk = () => {
+  console.log('add milk', );
+}
+
+const addIce = () => {
+  console.log('add ice');
+}
+
+const addTapioca = () => {
+  console.log('add tapioca');
+}
+
+const addSyrup = () => {
+  console.log('add syrup');
+}
+
+const shake = () => {
+  console.log('shake');
+}
+
+const recipe = () => {
+
+   const _addMilk = new Promise((resolve, reject) => {
+     addMilk();
+   });
+   const _addIce = new Promise((resolve, reject) => {
+     addIce();
+   });
+
+  Promise.all([_addMilk, _addIce])
+  .then(addTapioca())
+  .then(addSyrup())
+  .then(shake());
+}
+
+recipe();

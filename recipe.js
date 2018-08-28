d8721d54544a33f71693c22aa81894f7d697b861
uinjei <javier.edwinjay@gmail.com>
2018-08-28 19:10:03 +0800

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -1,32 +1,16 @@
-const addMilk = () => {
-  console.log('add milk', );
-}
-
-const addIce = () => {
-  console.log('add ice');
-}
-
-const addTapioca = () => {
-  console.log('add tapioca');
-}
-
-const addSyrup = () => {
-  console.log('add syrup');
-}
-
-const shake = () => {
-  console.log('shake');
-}
+const addMilk = () => console.log('add milk', );
+const addIce = () => console.log('add ice');
+const addTapioca = () => console.log('add tapioca');
+const addSyrup = () => console.log('add syrup');
+const shake = () => console.log('shake');
 
 const recipe = () => {
-
    const _addMilk = new Promise((resolve, reject) => {
      addMilk();
    });
    const _addIce = new Promise((resolve, reject) => {
      addIce();
    });
-
   Promise.all([_addMilk, _addIce])
   .then(addTapioca())
   .then(addSyrup())

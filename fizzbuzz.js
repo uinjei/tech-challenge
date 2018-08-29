5c677d5a61c9fe1d231ff8a5c99ba3c08e68d780
uinjei <javier.edwinjay@gmail.com>
2018-08-29 19:00:04 +0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,16 @@
 var msg;
-for(var i=1;i<=100;i++){
-    var msg="";
-    if (!(i%3)) msg+="Fizz"; 
-    if (!(i%5)) msg+="Buzz"; 
+
+const FIZZ = 'Fizz';
+const BUZZ = 'Buzz';
+const N = 100;
+
+for(let i=1;i<=N;i++){
+    let msg="";
+    if (!(i%3)) {
+      msg = msg + FIZZ;
+    } 
+    if (!(i%5)) {
+      msg = msg + BUZZ;
+    } 
     console.log(msg?msg:i); 
 }

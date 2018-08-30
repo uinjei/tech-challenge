637b233a170bd5802c5e5b570572613c78ce36c6
uinjei <javier.edwinjay@gmail.com>
2018-08-30 15:03:13 +0800

    Create es5_es6_obj.js

---
+++ es5_es6_obj.js
@@ -0,0 +1,21 @@
+// ES6
+const es6Obj = {
+  name: "Sam",
+  placesVisited: ["Manila", "Pasig", "Mandaluyong", "Taguig"], 
+  f() {
+    this.placesVisited.forEach(place => console.log(this.name,' has visited ', place));
+  }
+}
+
+es6Obj.f();
+
+// ES5
+var es5Obj = {
+  name: "Sam",
+  placesVisited: ["Manila", "Pasig", "Mandaluyong", "Taguig"], 
+  f: function(){
+    this.placesVisited.forEach(place => console.log(this.name,' has visited ', place));
+  }
+}
+
+es5Obj.f();
